a797e2790ff4be8fd8d5a1110f15e09004dd4d61
khornSaokhouch <khornsaokhouch4456@gmail.com>
2025-08-13 16:45:04 +0700

    fix

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -5,12 +5,15 @@ set -e
 
 # Run Laravel optimizations
 php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
 # Run database migrations
-php artisan migrate --force
+php artisan migrate --force || true
 
-# Start Laravel server
-php artisan serve --host=0.0.0.0 --port=8000
+# Start PHP-FPM (foreground) and Nginx
+php-fpm -F -R & nginx -g "daemon off;"
